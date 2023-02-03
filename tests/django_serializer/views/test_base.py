aeb0ce24b6a10393ec1787265ce35c22d7ae9548
overlucker <vlyankov@mail.ru>
2023-02-04 01:09:42 +0300

    rewrite tags and errors checks

--- tests/django_serializer/views/test_base.py
+++ tests/django_serializer/views/test_base.py
@@ -1,4 +1,7 @@
+from typing import Sequence, Type, Union
+
 import pytest
+from django.core import checks
 from django.forms import Form
 
 from django_serializer.v2.exceptions import (
@@ -11,18 +14,24 @@ from django_serializer.v2.views import ApiView, HttpMethod
 
 class TestMeta:
     @staticmethod
-    def _create_meta(attrs):
+    def _create_view(attrs)  -> Type[ApiView]:
+        class View(ApiView):
+            class Meta:
+                for k, v in attrs.items():
+                    locals()[k] = v
+
+        return View
+
+    @staticmethod
+    def _create_meta(attrs) -> Sequence[str]:
         try:
-            class View(ApiView):
-                class Meta:
-                    for k, v in attrs.items():
-                        locals()[k] = v
+            TestMeta._create_view(attrs)
         except IncorrectMetaException as e:
             return e.errors
 
     def test_empty(self):
         errors = self._create_meta({})
-        assert errors == ['`tags` is required', '`method` is required']
+        assert errors == ['`method` is required']
 
     def test_method(self):
         errors = self._create_meta({'tags': ['tags'], 'method': 'get'})
@@ -31,14 +40,58 @@ class TestMeta:
         ]
 
     @pytest.mark.parametrize('tags, expected_errors', [
-        (None, ['`tags` is required']),
-        ([], ['`tags` is required']),
-        ([1], ['`tags` item has incorrect type, should be str']),
-        (['t'], None),
+        (None, [
+            checks.Warning(
+                f'`View.Meta.tags` variable has incorrect type {type(None)}',
+                hint='View.Meta.tags should be list or tuple',
+                id='django_serializer.meta.tags.W001'
+            )
+        ]),
+        ([1], [
+            checks.Warning(
+                f'`View.Meta.tags[0]` variable has incorrect type {type(1)}',
+                hint='View.Meta.tags[0] should be str',
+                id='django_serializer.meta.tags.W002'
+            )
+        ]),
+        (['t'], []),
     ])
     def test_tags(self, tags, expected_errors):
-        errors = self._create_meta({'tags': tags, 'method': HttpMethod.GET})
-        assert errors == expected_errors
+        view = self._create_view({'tags': tags, 'method': HttpMethod.GET})
+
+        for error in expected_errors:
+            error.obj = view
+        assert view.check() == expected_errors
+
+    @pytest.mark.parametrize('errors, expected_errors', [
+        (None, [],),
+        (1, [
+            checks.Error(
+                f'`View.Meta.errors` variable has incorrect type {type(1)}',
+                hint='View.Meta.errors should be list or tuple',
+                id='django_serializer.meta.errors.E001'
+            )
+        ]),
+        ([Exception, 1], [
+            checks.Error(
+                f'`View.Meta.errors[0]` variable has incorrect type {type(Exception)}',
+                hint='`View.Meta.errors[0]` should be subclass of django_serializer.v2.exceptions.http.HttpError',
+                id='django_serializer.meta.errors.E002'
+            ),
+            checks.Error(
+                f'`View.Meta.errors[1]` variable has incorrect type {type(1)}',
+                hint='`View.Meta.errors[1]` should be subclass of django_serializer.v2.exceptions.http.HttpError',
+                id='django_serializer.meta.errors.E002'
+            )
+        ]),
+        ([BadRequestError], []),
+    ])
+    def test_errors(self, errors, expected_errors):
+        view = self._create_view({'errors': errors, 'method': HttpMethod.GET})
+
+        for error in expected_errors:
+            error.obj = view
+        assert view.check() == expected_errors
 
     @pytest.mark.parametrize('field, value, expected_errors', [
         ('summary', 1, ['`summary` has incorrect type, '
@@ -87,9 +140,6 @@ class TestMeta:
                                   "should be `<class 'bool'>`"]),
         ('serializer_many', True, None),
 
-        ('errors', [Exception, 1], ['`errors` item has incorrect type, '
-                                    'should be subtype of HttpError']),
-        ('errors', [BadRequestError], None),
     ])
     def test_var(self, field, value, expected_errors):
         errors = self._create_meta({'tags': ['tags'], 'method': HttpMethod.GET,
